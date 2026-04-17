1e9e77f4ebcefb7e4ebaa57a5e59ee971969bf67
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-04-16 21:17:07 -0500

    Parte 2 y parte 3: creacion de variables y contructor

--- src/com/krakedev/maquinaAliens/Aliens.java
+++ src/com/krakedev/maquinaAliens/Aliens.java
@@ -2,4 +2,27 @@ package com.krakedev.maquinaAliens;
 
 public class Aliens {
 
+	private int tamanio;
+    private String color;
+    private int numeroOjos;
+    private int numeroBrazos;
+    private int numeroPies;
+    private double precioExtremidad;
+    private double precioOjo;
+    private double precioCuerpo;
+
+    public Aliens(int tamanio, String color) {
+        if(tamanio < 5){
+            this.tamanio = 5;
+        }else if(tamanio > 30){
+            this.tamanio = 30;
+        }else{
+            this.tamanio = tamanio;
+        }
+        this.color = color;
+
+        this.precioCuerpo = 0.2 * this.tamanio;
+        this.precioExtremidad = 0.1 * this.tamanio;
+        this.precioOjo = 0.05 * this.tamanio;
+    }
 }
